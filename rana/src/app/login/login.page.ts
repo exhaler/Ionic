e4fe06e23eda1b1c71c8ddfdce4dc5adcb0dd05b
Fadi Barakat <fadibarakat@khoolood.com>
2020-04-18 17:57:57 +0300

    CanActivate gaurd

--- rana/src/app/login/login.page.ts
+++ rana/src/app/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { LoginCredential } from '../types';
 import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,6 +13,7 @@ export class LoginPage implements OnInit {
   loginFormGroup: FormGroup;
 
   constructor(
+    private _router: Router,
     private _loginService: LoginService,
     formBuilder: FormBuilder
   ) {
@@ -28,11 +30,11 @@ export class LoginPage implements OnInit {
     const loginCredentials: LoginCredential = this.loginFormGroup.value;
     this._loginService.login(loginCredentials)
     .then((authData)=> {
+      this._router.navigate(["/tabs"]);
       console.log(authData);
     })
     .catch((authError)=> {
       console.log("Auth Error => ", authError);
     });
   }
-
 }
