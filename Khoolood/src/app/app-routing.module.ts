2c96c6e28f13053e8bb9368c1b06436547cb6b7c
Fadi Barakat <fadibarakat@khoolood.com>
2020-04-25 14:01:01 +0300

    Added auth guard

--- Khoolood/src/app/app-routing.module.ts
+++ Khoolood/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
@@ -16,6 +17,8 @@ const routes: Routes = [
     path: "app",
     loadChildren: () =>
       import("./tabs/tabs.module").then((m) => m.TabsPageModule),
+      canLoad: [AuthGuard]
+      
   },
   {
     path: 'saved',
--- Khoolood/src/app/auth/auth.page.ts
+++ Khoolood/src/app/auth/auth.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+
+import { AuthService } from './auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +11,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+  isLoading = false;
+  isLogin = true;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private loadingCtrl: LoadingController
+  ) { }
 
   ngOnInit() {
+  }
+
+  onLogin() {
+    this.authService.login();
+    this.isLoading = true;
+    this.loadingCtrl
+    .create({
+      keyboardClose: true,
+      message: 'Logging in..'
+    })
+    .then(loadingEl => {
+      loadingEl.present();
+      setTimeout(() => {
+        this.isLoading = false;
+        loadingEl.dismiss();
+        this.router.navigateByUrl('/app/home')
+      }, 500);
+    });
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+
+    if (this.isLogin) {
+      // Send request to API to login
+    } else {
+      // Send request to API to signup
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 
 }
