d6965821b17b15fc703991f4e2186346183b33a8
Fadi Barakat <fadibarakat@khoolood.com>
2020-04-25 22:04:59 +0300

    Added subscriptions

--- PairBnB/src/app/places/offers/edit-offer/edit-offer.page.ts
+++ PairBnB/src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,54 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import { Place } from '../../place.model';
-import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
-import { PlacesService } from '../../places.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Place } from "../../place.model";
+import { ActivatedRoute } from "@angular/router";
+import { NavController } from "@ionic/angular";
+import { PlacesService } from "../../places.service";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-edit-offer',
-  templateUrl: './edit-offer.page.html',
-  styleUrls: ['./edit-offer.page.scss'],
+  selector: "app-edit-offer",
+  templateUrl: "./edit-offer.page.html",
+  styleUrls: ["./edit-offer.page.scss"],
 })
-export class EditOfferPage implements OnInit {
+export class EditOfferPage implements OnInit, OnDestroy {
   place: Place;
   form: FormGroup;
+  private placeSub: Subscription;
 
   constructor(
     private route: ActivatedRoute,
     private navCtrl: NavController,
     private placesService: PlacesService
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(paramMap => {
-      if (!paramMap.has('placeId')) {
-        this.navCtrl.navigateBack('/places/tabs/offers');
+    this.route.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has("placeId")) {
+        this.navCtrl.navigateBack("/places/tabs/offers");
         return;
       }
-      this.place = this.placesService.getPlace(paramMap.get('placeId'));
-      this.form = new FormGroup({
-        title: new FormControl(this.place.title, {
-          updateOn: 'blur',
-          validators:  [
-            Validators.required
-          ]
-        }),
-        description: new FormControl(this.place.description, {
-          updateOn: 'blur',
-          validators:  [
-            Validators.required,
-            Validators.maxLength(180)
-          ]
-        })
-      })
+      this.placeSub = this.placesService
+        .getPlace(paramMap.get("placeId"))
+        .subscribe((place) => {
+          this.place = place;
+
+          this.form = new FormGroup({
+            title: new FormControl(this.place.title, {
+              updateOn: "blur",
+              validators: [Validators.required],
+            }),
+            description: new FormControl(this.place.description, {
+              updateOn: "blur",
+              validators: [Validators.required, Validators.maxLength(180)],
+            }),
+          });
+        });
     });
   }
 
   onUpdateOffer() {
-    if(!this.form.valid) {
+    if (!this.form.valid) {
       return;
     }
-    console.log(this.form)
+    console.log(this.form);
+  }
+
+  ngOnDestroy() {
+    if (this.placeSub) {
+      this.placeSub.unsubscribe();
+    }
   }
 }
--- PairBnB/src/app/places/places.service.ts
+++ PairBnB/src/app/places/places.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from "@angular/core";
 import { Place } from "./place.model";
+import { AuthService } from "../auth/auth.service";
+import { BehaviorSubject } from "rxjs";
+import { take, map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
 })
 export class PlacesService {
-  private _places: Place[] = [
+  private _places = new BehaviorSubject<Place[]>([
     new Place(
       "p1",
       "Manhattan Mansion",
       "In the hear of New York City.",
       "https://static3.mansionglobal.com/production/media/article-images/3f32c767fc327e62abb28a2f9dc8d4a5/large_GettyImages-637703666.jpg",
       149.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31')
+      new Date("2019-01-01"),
+      new Date("2019-12-31"),
+      "abc"
     ),
     new Place(
       "p2",
@@ -21,8 +25,9 @@ export class PlacesService {
       "A romantic place in Paris!",
       "https://a0.muscache.com/im/pictures/66092977/1911951d_original.jpg?aki_policy=xx_large",
       189.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31')
+      new Date("2019-01-01"),
+      new Date("2019-12-31"),
+      "abc"
     ),
     new Place(
       "p3",
@@ -30,19 +35,47 @@ export class PlacesService {
       "Not you average city trip!",
       "https://cdn.theculturetrip.com/wp-content/uploads/2016/04/screenshot-958.png",
       99.99,
-      new Date('2019-01-01'),
-      new Date('2019-12-31')
+      new Date("2019-01-01"),
+      new Date("2019-12-31"),
+      "abc"
     ),
-  ];
+  ]);
 
   get places() {
-    return [...this._places];
+    return this._places.asObservable();
   }
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   getPlace(id: string) {
-    return {...this._places.find(p => p.id ===id)};
+    return this.places.pipe(
+      take(1),
+      map((places) => {
+        return { ...places.find((p) => p.id === id) };
+      })
+    );
   }
 
+  addPlace(
+    title: string,
+    description: string,
+    price: number,
+    dateFrom: Date,
+    dateTo: Date
+  ) {
+    const newPlace = new Place(
+      Math.random().toString(),
+      title,
+      description,
+      "https://cdn.theculturetrip.com/wp-content/uploads/2016/04/screenshot-958.png",
+      price,
+      dateFrom,
+      dateTo,
+      this.authService.userId
+    );
+
+    this.places.pipe(take(1)).subscribe((places) => {
+      this._places.next(places.concat(newPlace));
+    });
+  }
 }
