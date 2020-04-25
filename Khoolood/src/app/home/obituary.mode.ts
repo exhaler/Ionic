42fb0a34962ac2171bfd60161ce8988e6ccc7168
Fadi Barakat <fadibarakat@khoolood.com>
2020-04-25 19:13:44 +0300

    Added obituary class

---
+++ Khoolood/src/app/home/obituary.mode.ts
@@ -0,0 +1,8 @@
+export class Obituary {
+    constructor(
+        public id: string,
+        public title: string,
+        public description: string,
+        public imageUrl: string,
+    ) {}
+}
