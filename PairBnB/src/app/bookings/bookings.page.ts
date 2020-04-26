f7031bbaf759fd000636c2530a942b412c9b5c7a
Fadi Barakat <fadibarakat@khoolood.com>
2020-04-26 20:32:08 +0300

    Show your bookings

--- PairBnB/src/app/bookings/bookings.page.ts
+++ PairBnB/src/app/bookings/bookings.page.ts
@@ -1,28 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { IonItemSliding } from '@ionic/angular';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { IonItemSliding, LoadingController } from "@ionic/angular";
 
-import { BookingService } from './booking.service';
-import { Booking } from './booking.model';
+import { BookingService } from "./booking.service";
+import { Booking } from "./booking.model";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-bookings',
-  templateUrl: './bookings.page.html',
-  styleUrls: ['./bookings.page.scss'],
+  selector: "app-bookings",
+  templateUrl: "./bookings.page.html",
+  styleUrls: ["./bookings.page.scss"],
 })
-export class BookingsPage implements OnInit {
+export class BookingsPage implements OnInit, OnDestroy {
   loadedBookings: Booking[];
+  private bookingSub: Subscription;
 
   constructor(
-    private bookingService: BookingService
-  ) { }
+    private bookingService: BookingService,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit() {
-    this.loadedBookings = this.bookingService.bookings;
+    this.bookingSub = this.bookingService.bookings.subscribe((bookings) => {
+      this.loadedBookings = bookings;
+    });
   }
 
-  onCancelBooking(offerId: string, slidingEl: IonItemSliding) {
+  onCancelBooking(bookingId: string, slidingEl: IonItemSliding) {
     slidingEl.close();
-    // cancel booking with id
+    this.loadingCtrl.create({ message: "Cancelling..." }).then((loadingEl) => {
+      loadingEl.present();
+      this.bookingService.cancelBooking(bookingId).subscribe(() => {
+        loadingEl.dismiss();
+      });
+    });
   }
 
+  ngOnDestroy() {
+    if (this.bookingSub) {
+      this.bookingSub.unsubscribe();
+    }
+  }
 }
--- PairBnB/src/app/places/discover/place-detail/place-detail.page.ts
+++ PairBnB/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { NavController, ModalController, ActionSheetController } from "@ionic/angular";
+import {
+  NavController,
+  ModalController,
+  ActionSheetController,
+  LoadingController,
+} from "@ionic/angular";
 
 import { Place } from "../../place.model";
 import { CreateBookingComponent } from "../../../bookings/create-booking/create-booking.component";
 import { PlacesService } from "../../places.service";
-import { Subscription } from 'rxjs';
+import { Subscription } from "rxjs";
+import { BookingService } from "../../../bookings/booking.service";
 
 @Component({
   selector: "app-place-detail",
@@ -21,7 +27,9 @@ export class PlaceDetailPage implements OnInit, OnDestroy {
     private navCtrl: NavController,
     private modalCtrl: ModalController,
     private placesService: PlacesService,
-    private actionSheetCtrl: ActionSheetController
+    private actionSheetCtrl: ActionSheetController,
+    private bookingService: BookingService,
+    private loadingCtrl: LoadingController
   ) {}
 
   ngOnInit() {
@@ -30,40 +38,43 @@ export class PlaceDetailPage implements OnInit, OnDestroy {
         this.navCtrl.navigateBack("/places/tabs/discover");
         return;
       }
-      this.placeSub = this.placesService.getPlace(paramMap.get("placeId")).subscribe(place => {
-        this.place = place;
-      });
+      this.placeSub = this.placesService
+        .getPlace(paramMap.get("placeId"))
+        .subscribe((place) => {
+          this.place = place;
+        });
     });
   }
 
   onBookPlace() {
-    this.actionSheetCtrl.create({
-      header: 'Choose an action',
-      buttons: [
-        {
-          text: 'Select Date',
-          handler: () => {
-            this.openBookingModal('select');
-          }
-        },
-        {
-          text: 'Random Date',
-          handler: () => {
-            this.openBookingModal('random');
-          }
-        },
-        {
-          text: 'Cancel',
-          role: 'cancel'
-        }
-      ]
-    })
-    .then(actionSheetEl => {
-      actionSheetEl.present();
-    })
+    this.actionSheetCtrl
+      .create({
+        header: "Choose an action",
+        buttons: [
+          {
+            text: "Select Date",
+            handler: () => {
+              this.openBookingModal("select");
+            },
+          },
+          {
+            text: "Random Date",
+            handler: () => {
+              this.openBookingModal("random");
+            },
+          },
+          {
+            text: "Cancel",
+            role: "cancel",
+          },
+        ],
+      })
+      .then((actionSheetEl) => {
+        actionSheetEl.present();
+      });
   }
 
-  openBookingModal(mode: 'select' | 'random') {
+  openBookingModal(mode: "select" | "random") {
     console.log(mode);
     this.modalCtrl
       .create({
@@ -74,16 +85,34 @@ export class PlaceDetailPage implements OnInit, OnDestroy {
         modelEl.present();
         return modelEl.onDidDismiss();
       })
-      .then(resultData => {
-        console.log(resultData.data, resultData.role);
-        if (resultData.role === 'confirm') {
-          console.log('BOOKED!');
+      .then((resultData) => {
+        if (resultData.role === "confirm") {
+          this.loadingCtrl
+            .create({ message: "Booking place..." })
+            .then((loadingEl) => {
+              loadingEl.present();
+              const data = resultData.data.bookingData;
+              this.bookingService
+                .addBooking(
+                  this.place.id,
+                  this.place.title,
+                  this.place.imageUrl,
+                  data.firstName,
+                  data.lastName,
+                  data.guestNumber,
+                  data.startDate,
+                  data.endDate
+                )
+                .subscribe(() => {
+                  loadingEl.dismiss();
+                });
+            });
         }
-      })
+      });
   }
 
   ngOnDestroy() {
-    if(this.placeSub) {
+    if (this.placeSub) {
       this.placeSub.unsubscribe();
     }
   }
