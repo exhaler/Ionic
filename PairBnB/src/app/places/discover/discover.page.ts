2684df3473e0a69a3964963cece23b973b23cfe7
Fadi Barakat <fadibarakat@khoolood.com>
2020-04-26 17:50:34 +0300

    Show message when switching to bookable segment

--- PairBnB/src/app/places/discover/discover.page.ts
+++ PairBnB/src/app/places/discover/discover.page.ts
@@ -1,33 +1,45 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { SegmentChangeEventDetail } from '@ionic/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { SegmentChangeEventDetail } from "@ionic/core";
+import { Subscription } from "rxjs";
 
-import { PlacesService } from '../places.service';
-import { Place } from '../place.model';
-import { Subscription } from 'rxjs';
+import { PlacesService } from "../places.service";
+import { Place } from "../place.model";
+import { AuthService } from "../../auth/auth.service";
 
 @Component({
-  selector: 'app-discover',
-  templateUrl: './discover.page.html',
-  styleUrls: ['./discover.page.scss'],
+  selector: "app-discover",
+  templateUrl: "./discover.page.html",
+  styleUrls: ["./discover.page.scss"],
 })
 export class DiscoverPage implements OnInit, OnDestroy {
   loadedPlaces: Place[];
   listedLoadedPlaces: Place[];
+  relevantPlaces: Place[];
   private placesSub: Subscription;
 
   constructor(
-    private placesService: PlacesService
-  ) { }
+    private placesService: PlacesService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
-    this.placesSub = this.placesService.places.subscribe(places => {
+    this.placesSub = this.placesService.places.subscribe((places) => {
       this.loadedPlaces = places;
-      this.listedLoadedPlaces = this.loadedPlaces.slice(1);
+      this.relevantPlaces = this.loadedPlaces;
+      this.listedLoadedPlaces = this.relevantPlaces.slice(1);
     });
   }
 
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
-    console.log(event.detail);
+    if (event.detail.value === "all") {
+      this.relevantPlaces = this.loadedPlaces;
+      this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+    } else {
+      this.relevantPlaces = this.loadedPlaces.filter(
+        (place) => place.userId !== this.authService.userId
+      );
+      this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+    }
   }
 
   ngOnDestroy() {
@@ -35,5 +47,4 @@ export class DiscoverPage implements OnInit, OnDestroy {
       this.placesSub.unsubscribe();
     }
   }
-
 }
