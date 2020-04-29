22077ff46327edae2e5cec6ae5f8b90e2246454b
Fadi Barakat <fadibarakat@khoolood.com>
2020-04-29 17:43:15 +0300

    Removed used page and added loading  handler

--- PairBnB/src/app/places/discover/place-detail/place-detail.page.ts
+++ PairBnB/src/app/places/discover/place-detail/place-detail.page.ts
@@ -22,6 +22,7 @@ import { AuthService } from '../../../auth/auth.service';
 export class PlaceDetailPage implements OnInit, OnDestroy {
   place: Place;
   isBookable = false;
+  isLoading = false;
   private placeSub: Subscription;
 
   constructor(
@@ -41,11 +42,14 @@ export class PlaceDetailPage implements OnInit, OnDestroy {
         this.navCtrl.navigateBack("/places/tabs/discover");
         return;
       }
+      this.isLoading = true;
+
       this.placeSub = this.placesService
         .getPlace(paramMap.get("placeId"))
         .subscribe((place) => {
           this.place = place;
           this.isBookable = place.userId !== this.authService.userId;
+          this.isLoading = false;
         });
     });
   }
