18d9bc526fed137a6c7f86218ade830fcf21c66d
Fadi Barakat <fadibarakat@khoolood.com>
2020-04-30 16:21:48 +0300

    Show image when picking a locaiton

--- PairBnB/src/app/shared/pickers/location-picker/location-picker.component.ts
+++ PairBnB/src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit, ɵdevModeEqual } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 import { ModalController } from "@ionic/angular";
 
-import { map } from "rxjs/operators";
+import { map, switchMap } from "rxjs/operators";
 
 import { environment } from "../../../../environments/environment";
 import { MapModalComponent } from "../../map-modal/map-modal.component";
+import { PlaceLocation } from "../../../places/location.model";
+import { of } from "rxjs";
 
 @Component({
   selector: "app-location-picker",
@@ -14,6 +16,9 @@ import { MapModalComponent } from "../../map-modal/map-modal.component";
   styleUrls: ["./location-picker.component.scss"],
 })
 export class LocationPickerComponent implements OnInit {
+  selectedLocationImage: string;
+  isLoading = false;
+
   constructor(private modalCtrl: ModalController, private http: HttpClient) {}
 
   ngOnInit() {}
@@ -28,12 +33,27 @@ export class LocationPickerComponent implements OnInit {
           if (!modalData.data) {
             return;
           }
-          this.getAddress(
-            modalData.data.lat,
-            modalData.data.lng
-          ).subscribe((address) => {
-            console.log(address);
-          });
+          const pickedLocation: PlaceLocation = {
+            lat: modalData.data.lat,
+            lng: modalData.data.lng,
+            address: null,
+            staticMapImageUrl: null,
+          };
+          this.isLoading = true;
+          this.getAddress(modalData.data.lat, modalData.data.lng)
+            .pipe(
+              switchMap((address) => {
+                pickedLocation.address = address;
+                return of(
+                  this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14)
+                );
+              })
+            )
+            .subscribe((staticMapImageUrl) => {
+              pickedLocation.staticMapImageUrl = staticMapImageUrl;
+              this.selectedLocationImage = staticMapImageUrl;
+              this.isLoading = false;
+            });
         });
         modalEl.present();
       });
@@ -53,5 +73,11 @@ export class LocationPickerComponent implements OnInit {
           return geoData.results[0].formatted_address;
         })
       );
+  }
+
+  private getMapImage(lat: number, lng: number, zoom: number) {
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&maptype=roadmap
+    &markers=color:red%7Clabel:Place%7C${lat},${lng}
+    &key=${environment.googleMapsAPIKey}`;
   }
 }
