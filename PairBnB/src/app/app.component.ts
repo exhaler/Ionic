d279421029a1ea378e752cafdf9eb1c320977bc4
Fadi Barakat <fadibarakat@khoolood.com>
2020-05-02 16:29:37 +0300

    npm capacitor for splash screen

--- PairBnB/src/app/app.component.ts
+++ PairBnB/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Platform } from '@ionic/angular';
 
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from '@ionic/angular';
+import { Plugins, Capacitor } from '@capacitor/core';
 
 import { AuthService } from './auth/auth.service';
 
@@ -16,8 +15,6 @@ import { AuthService } from './auth/auth.service';
 export class AppComponent {
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
     private authService: AuthService,
     private router: Router
   ) {
@@ -26,8 +23,9 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      if (Capacitor.isPluginAvailable('SplashScreen')) {
+        Plugins.SplashScreen.hide();
+      }
     });
   }
 
