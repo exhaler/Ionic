62adccab91563eb6801615f98c138274ce40e505
Fadi Barakat <fadibar@gmail.com>
2020-05-04 01:30:44 +0300

    Fixed auth gaurd

--- PairBnB/src/app/auth/auth.guard.ts
+++ PairBnB/src/app/auth/auth.guard.ts
@@ -1,23 +1,27 @@
-import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AuthService } from './auth.service';
+import { Injectable } from "@angular/core";
+import { CanLoad, Route, UrlSegment, Router } from "@angular/router";
+import { Observable } from "rxjs";
+import { take, tap } from "rxjs/operators";
+
+import { AuthService } from "./auth.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthGuard implements CanLoad {
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   canLoad(
     route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    if (!this.authService.userIsAuthenticated) {
-      this.router.navigateByUrl('/auth');
-    }
-    return this.authService.userIsAuthenticated;
+    segments: UrlSegment[]
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.userIsAuthenticated.pipe(
+      take(1),
+      tap((isAuthenticated) => {
+        if (!isAuthenticated) {
+          this.router.navigateByUrl("/auth");
+        }
+      })
+    );
   }
 }
--- PairBnB/src/app/bookings/booking.service.ts
+++ PairBnB/src/app/bookings/booking.service.ts
@@ -48,7 +48,7 @@ export class BookingService {
       take(1),
       switchMap((userId) => {
         if (!userId) {
-          throw new Error('No user id found');
+          throw new Error("No user id found");
         } else {
           newBooking = new Booking(
             Math.random().toString(),
@@ -98,36 +98,40 @@ export class BookingService {
   }
 
   fetchBookings() {
-    return this.http
-      .get<{ [key: string]: bookingData }>(
-        `${environment.firebaseURL}/bookings.json?orderBy="userId"&equalTo="${this.authService.userId}"`
-      )
-      .pipe(
-        map((bookingData) => {
-          const bookings = [];
-          for (const key in bookingData) {
-            if (bookingData.hasOwnProperty(key)) {
-              bookings.push(
-                new Booking(
-                  key,
-                  bookingData[key].placeId,
-                  bookingData[key].userId,
-                  bookingData[key].placeTitle,
-                  bookingData[key].placeImage,
-                  bookingData[key].firstName,
-                  bookingData[key].lastName,
-                  bookingData[key].guestNumber,
-                  new Date(bookingData[key].bookedFrom),
-                  new Date(bookingData[key].bookedTo)
-                )
-              );
-            }
+   return this.authService.userId.pipe(
+      switchMap((userId) => {
+        if (!userId) {
+          throw new Error("User id not found");
+        }
+        return this.http.get<{ [key: string]: bookingData }>(
+          `${environment.firebaseURL}/bookings.json?orderBy="userId"&equalTo="${userId}"`
+        );
+      }),
+      map((bookingData) => {
+        const bookings = [];
+        for (const key in bookingData) {
+          if (bookingData.hasOwnProperty(key)) {
+            bookings.push(
+              new Booking(
+                key,
+                bookingData[key].placeId,
+                bookingData[key].userId,
+                bookingData[key].placeTitle,
+                bookingData[key].placeImage,
+                bookingData[key].firstName,
+                bookingData[key].lastName,
+                bookingData[key].guestNumber,
+                new Date(bookingData[key].bookedFrom),
+                new Date(bookingData[key].bookedTo)
+              )
+            );
           }
-          return bookings;
-        }),
-        tap((bookings) => {
-          this._bookings.next(bookings);
-        })
-      );
+        }
+        return bookings;
+      }),
+      tap((bookings) => {
+        this._bookings.next(bookings);
+      })
+    );
   }
 }
