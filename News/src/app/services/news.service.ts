4fb7605ffb54b87b3be24fd7cc33f6f3ca960715
Fadi Barakat <fadibar@gmail.com>
2020-05-11 16:54:25 +0300

    Created components and added more info to cards

---
+++ News/src/app/services/news.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+
+const apiKey = environment.apiKey;
+const apiUrl = environment.apiUrl;
+const params = new HttpParams().set('apiKey', apiKey);
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  constructor(private http: HttpClient) { }
+
+  getData(url) {
+    return this.http.get(`${apiUrl}/${url}`, {params})
+  }
+}
+ 
--- News/src/app/top-news/top-news.page.ts
+++ News/src/app/top-news/top-news.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NewsService } from "../services/news.service";
 
 @Component({
-  selector: 'app-top-news',
-  templateUrl: './top-news.page.html',
-  styleUrls: ['./top-news.page.scss'],
+  selector: "app-top-news",
+  templateUrl: "./top-news.page.html",
+  styleUrls: ["./top-news.page.scss"],
 })
 export class TopNewsPage implements OnInit {
+  news: any;
 
-  constructor() { }
+  constructor(private newService: NewsService) {}
 
   ngOnInit() {
+    // change country to geolocation
+    this.news = this.newService.getData("top-headlines?country=us");
   }
-
 }
