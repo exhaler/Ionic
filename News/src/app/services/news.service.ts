b9125e4615404aa441d9d4f96315be89a9d1f7f9
Fadi Barakat <fadibar@gmail.com>
2020-05-11 17:29:20 +0300

    Added a loader

--- News/src/app/services/news.service.ts
+++ News/src/app/services/news.service.ts
@@ -1,21 +1,40 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
 
-import { environment } from '../../environments/environment';
+import { tap } from "rxjs/operators";
+
+import { LoadingController } from "@ionic/angular";
+
+import { environment } from "../../environments/environment";
 
 const apiKey = environment.apiKey;
 const apiUrl = environment.apiUrl;
-const params = new HttpParams().set('apiKey', apiKey);
+const params = new HttpParams().set("apiKey", apiKey);
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class NewsService {
+  loading;
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    public loadingCtrl: LoadingController
+  ) {}
 
   getData(url) {
-    return this.http.get(`${apiUrl}/${url}`, {params})
+    this.showLoading();
+    return this.http.get(`${apiUrl}/${url}`, { params }).pipe(tap(value => {
+      this.loading.dismiss();
+    }));
+  }
+
+  async showLoading() {
+    this.loading = await this.loadingCtrl.create({
+      duration: 5000,
+    });
+
+    return await this.loading.present();
   }
 }
  
