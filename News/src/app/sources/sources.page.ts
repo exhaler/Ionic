1ccdd1bfa4e1aedfcf21a25bf08157e669a57500
Fadi Barakat <fadibar@gmail.com>
2020-05-11 21:14:27 +0300

    Pipes and filter source

--- News/src/app/sources/sources.page.ts
+++ News/src/app/sources/sources.page.ts
@@ -9,11 +9,14 @@ import { NewsService } from '../services/news.service';
 })
 export class SourcesPage implements OnInit {
   sources;
+  term = "";
 
   constructor(private newsService: NewsService) { }
 
   ngOnInit() {
-    this.sources = this.newsService.getData('sources');
+    this.newsService.getData('sources').subscribe(sources => {
+      this.sources = sources['sources'];
+    })
   }
 
 }
