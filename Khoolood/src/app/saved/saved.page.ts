532cf7202409e0ed2c33d66eb0f76194c76dd62d
Fadi Barakat <fadibar@gmail.com>
2020-05-15 20:16:44 +0300

    Show saved items

--- Khoolood/src/app/saved/saved.page.ts
+++ Khoolood/src/app/saved/saved.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Storage } from "@ionic/storage";
+
+import { SaveService } from "../shared/services/save.service";
+import { DetailedObituaryObject } from '../shared/models';
 
 @Component({
-  selector: 'app-saved',
-  templateUrl: './saved.page.html',
-  styleUrls: ['./saved.page.scss'],
+  selector: "app-saved",
+  templateUrl: "./saved.page.html",
+  styleUrls: ["./saved.page.scss"],
 })
 export class SavedPage implements OnInit {
+  savedObituaries: DetailedObituaryObject[] = [];
+  
+  constructor(private saveService: SaveService, public storage: Storage) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    this.saveService.getAllSavedItems().then((data) => {
+      this.savedObituaries = data;
+    });
   }
-
 }
