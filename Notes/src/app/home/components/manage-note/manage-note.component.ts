9da0614eed0641fd1a117de0a1991ebc193383ec
Fadi Barakat <fadibar@gmail.com>
2020-05-16 23:13:17 +0300

    Added a form in the moda

--- Notes/src/app/home/components/manage-note/manage-note.component.ts
+++ Notes/src/app/home/components/manage-note/manage-note.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { ModalController } from '@ionic/angular';
+import { ModalController } from "@ionic/angular";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-manage-note',
-  templateUrl: './manage-note.component.html',
-  styleUrls: ['./manage-note.component.scss'],
+  selector: "app-manage-note",
+  templateUrl: "./manage-note.component.html",
+  styleUrls: ["./manage-note.component.scss"],
 })
 export class ManageNoteComponent implements OnInit {
+  noteForm: FormGroup;
 
-  constructor(private modalCtrl: ModalController) { }
+  constructor(private modalCtrl: ModalController, private fb: FormBuilder) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.noteForm = this.fb.group({
+      title: ["", [Validators.required]],
+      description: ["", []],
+    });
+  }
+
+  formSubmit() {
+    console.log(this.noteForm.value)
+  }
 
   dismiss() {
     this.modalCtrl.dismiss();
   }
-
 }
