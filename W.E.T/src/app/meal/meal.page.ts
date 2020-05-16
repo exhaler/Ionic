c835076e5cd16a25c3d8acc9d9aacb19f8b36c33
Fadi Barakat <fadibar@gmail.com>
2020-05-16 12:56:55 +0300

    Changed sharing options

--- W.E.T/src/app/meal/meal.page.ts
+++ W.E.T/src/app/meal/meal.page.ts
@@ -79,17 +79,17 @@ export class MealPage implements OnInit {
   }
 
   shareMeal(meal) {
-    // var options = {
-    //   message: 'share this', // not supported on some apps (Facebook, Instagram)
-    //   subject: 'the subject', // fi. for email
-    //   files: ['', ''], // an array of filenames either locally or remotely
-    //   url: 'https://www.website.com/foo/#bar?a=b',
-    //   chooserTitle: 'Pick an app', // Android only, you can override the default share sheet title
-    //   appPackageName: 'com.apple.social.facebook', // Android only, you can provide id of the App you want to share with
-    //   iPadCoordinates: '0,0,0,0' //IOS only iPadCoordinates for where the popover should be point.  Format with x,y,width,height
-    // };
-    // this.socialSharing.shareWithOptions(options);
-    this.socialSharing.share("Check out this delicious meal", meal.strMeal, meal.strMealThumb, null);
+    var options = {
+      message: 'Check out this delicious meal', // not supported on some apps (Facebook, Instagram)
+      subject: meal.strMeal, // fi. for email
+      files: [meal.strMealThumb], // an array of filenames either locally or remotely
+      //url: 'https://www.website.com/foo/#bar?a=b',
+      //chooserTitle: 'Pick an app', // Android only, you can override the default share sheet title
+      //appPackageName: 'com.apple.social.facebook', // Android only, you can provide id of the App you want to share with
+      //iPadCoordinates: '0,0,0,0' //IOS only iPadCoordinates for where the popover should be point.  Format with x,y,width,height
+    };
+    this.socialSharing.shareWithOptions(options);
+    //this.socialSharing.share("Check out this delicious meal", meal.strMeal, meal.strMealThumb, null);
 
   }
 }
