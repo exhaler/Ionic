6321dc89eb620642f236afd3e7feab589aef1da0
Fadi Barakat <fadibar@gmail.com>
2020-05-22 21:11:10 +0300

    Updated notes model to have image and reminder time

--- Notes/src/app/core/models/note.model.ts
+++ Notes/src/app/core/models/note.model.ts
@@ -1,10 +1,12 @@
-import { observable, action } from 'mobx-angular';
+import { observable, action } from "mobx-angular";
 
 export interface INote {
   id: number;
   title: string;
   description: string;
   archived: boolean;
+  imagePath: string;
+  reminderTime?: string;
 }
 
 export class Note implements INote {
@@ -12,16 +14,30 @@ export class Note implements INote {
   title: string;
   description: string;
   @observable archived: boolean;
+  @observable imagePath: string;
+  @observable reminderTime: string;
 
   constructor(note: Partial<INote>) {
     this.id = note.id ? note.id : new Date().getTime();
     this.title = note.title ? note.title : "";
     this.description = note.description ? note.description : "";
-    this.setArchived(this.archived = note.archived !== undefined ? note.archived : false);
+    this.setArchived(note.archived !== undefined ? note.archived : false);
+    this.setImagePath(note.imagePath ? note.imagePath : "");
+    this.setReminderTime(note.reminderTime ? note.reminderTime : "");
   }
 
   @action
   setArchived(value) {
-    this.archived = value
+    this.archived = value;
+  }
+
+  @action
+  setImagePath(imagePath) {
+    this.imagePath = imagePath;
+  }
+
+  @action
+  setReminderTime(reminderTime) {
+    this.reminderTime = reminderTime;
   }
 }
